9d022f1d0f65b4ad8202aa4e6994c9d07da97f1b
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-05-11 21:29:36 +0800

    add admin layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  u = User.create([ email: 'admin@jolmarine.com' ,  password: '123456' , password_confirmation:'123456', is_admin: true])
+  puts "create an admin user"
+
+
+  products =[{title:"Ultra-Thin Touch Mouse T630",
+  	        description: "Designed for Ultrabooks
+                          Ultra-portable
+                          Windows 8 touch gesture support
+                          Bluetooth wireless with Logitech easy-switch technology
+                          Rechargeable",
+  	        image_path: "#{Rails.root}/app/assets/images/logitech_mouse.jpg"},
+
+  	        {title:"A Mid-Back Mesh Chair",
+  	        description: "Ergonomic office chair with contoured mesh back for breathability
+  	              Pneumatic seat-height adjustment; padded seat for comfort.",
+  	        image_path: "#{Rails.root}/app/assets/images/chair.jpg"}
+  ]
+
+
+    p1 = Product.create([title:products[0][:title],description:products[0][:description],quantity:20,price:30,image:open(products[0][:image_path])])
+    p2 = Product.create([title:products[1][:title],description:products[1][:description],quantity:20,price:30,image:open(products[1][:image_path])])
+    puts "create products"
